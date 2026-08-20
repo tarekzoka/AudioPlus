5f2fa9547f2574f7f17de4f015b379bc86957541
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-08-20 12:31:25 +0300

    Add files via upload

---
+++ AudioPlus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tarekzoka/AudioPlus/refs/heads/main/audioplus.sh -O - | /bin/sh
+##
+echo
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/audioplus.py https://raw.githubusercontent.com/tarekzoka/AudioPlus/refs/heads/main/audioplus.py
+sleep 2;
+exit 0  
