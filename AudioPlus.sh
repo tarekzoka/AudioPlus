2bba2f053b36de9cdeea5bed6e180d1efb28f6b4
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2026-08-21 00:46:47 +0300

    Delete AudioPlus.sh

--- AudioPlus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/AudioPlus/refs/heads/main/AudioPlus.sh -O - | /bin/sh
-##
-echo
-wget -O /usr/lib/enigma2/python/Plugins/Extensions/audioplus.py https://raw.githubusercontent.com/tarekzoka/AudioPlus/refs/heads/main/audioplus.py
-sleep 2;
-exit 0  
